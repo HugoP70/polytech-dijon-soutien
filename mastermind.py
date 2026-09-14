8534dea8b2eceb980887e4688c59eac7cd6e8c8f
HugoP70 <hugo_pernin@etu.u-bourgogne.fr>
2026-09-14 08:41:41 +0200

    feat: ajout du menu principal

---
+++ mastermind.py
@@ -0,0 +1,28 @@
+def menu(premierLancement):
+    print("----------------------------------------")
+    print("|              Mastermind              |")
+    print("----------------------------------------")
+    if premierLancement:
+        print("| 0 - Jouer                            |")
+    else:
+        print("| 0 - Rejouer                          |")
+    print ("| 1 - Remettre à zéro les statistiques |")
+    print ("| 2 - Quitter                          |")
+    print("----------------------------------------")
+    valeurOk = False
+    while not(valeurOk):
+        try:
+            choixUtilisateur = int(input(""))
+            match (choixUtilisateur):
+                case 0:
+                    valeurOk = True
+                case 1:
+                    valeurOk = True
+                case 2:
+                    valeurOk = True
+                case _:
+                    raise ValueError()
+        except ValueError:
+            print("Erreur : choix invalide")
+
+menu(True)
